61e0996d476ec61601b8e981da81b23b8f4a1928
Ph4nt0m37 <118580028+Ph4nt0m37@users.noreply.github.com>
2024-01-27 14:50:52 -0500

    push

--- Assets/Cam.cs
+++ Assets/Cam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Cam : MonoBehaviour
 {
     public GameObject player;
     // Start is called before the first frame update
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -19,10 +19,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //float hypotenuse = Vector3.Distance(cam.transform.position, Input.mousePosition);
-        float xDistance = cam.transform.position.x - Input.mousePosition.x;
-        float yDistance = cam.transform.position.y-Input.mousePosition.y;
-        //gameObject.transform.rotation = Quaternion.LookRotation(xDistance,yDistance);
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        difference.Normalize();
+
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += Vector3.right * 5f * Time.deltaTime;
