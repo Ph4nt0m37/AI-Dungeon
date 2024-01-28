39cd941f62a92fe4d5757d9b7ec766f0307184b4
Ph4nt0m37 <118580028+Ph4nt0m37@users.noreply.github.com>
2024-01-27 21:32:55 -0500

    stuff

--- Assets/SwingWeapon.cs
+++ Assets/SwingWeapon.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Swing : MonoBehaviour
+public class SwingWeapon : MonoBehaviour
 {
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void swing()
     {
-        animator.SetTrigger("Attacked");
         animator.SetTrigger("Swing");
     }
 }
